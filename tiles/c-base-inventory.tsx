9ecb51d6ee46a1678ba2f04f16240907c739fb69
Sven G. Brönstrup <broenstrup@spom.net>
2020-12-20 14:05:28 +0100

    Pimped up space

--- tiles/c-base-inventory.tsx
+++ tiles/c-base-inventory.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="c-base-inventory" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="../c-base-inventory.png" width="512" height="512"/>
+ <image source="../img/c-base-inventory.png" width="512" height="512"/>
  <tile id="0">
   <properties>
    <property name="collides" type="bool" value="true"/>
--- tiles/water.tsx
+++ tiles/water.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Water" tilewidth="32" tileheight="32" tilecount="10" columns="6">
- <image source="../water.png" width="192" height="32"/>
+<tileset version="1.4" tiledversion="1.4.3" name="Water" tilewidth="32" tileheight="32" tilecount="6" columns="6">
+ <image source="../img/water.png" width="192" height="32"/>
  <tile id="0">
   <properties>
    <property name="collides" type="bool" value="false"/>
