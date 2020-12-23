a723aa230488343fe740cb509471a06666d8e457
Sven G. Brönstrup <broenstrup@spom.net>
2020-12-23 23:09:37 +0100

    Optimized and integrated reactor

--- tiles/c-base-reactor.tsx
+++ tiles/c-base-reactor.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="c-base-reactor" tilewidth="32" tileheight="32" tilecount="16" columns="4">
- <image source="../img/c-base-reactor.png" width="128" height="128"/>
+<tileset version="1.4" tiledversion="1.4.3" name="c-base-reactor" tilewidth="32" tileheight="32" tilecount="32" columns="4">
+ <image source="../img/c-base-reactor.png" width="128" height="256"/>
  <tile id="0">
   <properties>
    <property name="collides" type="bool" value="true"/>
@@ -32,6 +32,16 @@
   </properties>
  </tile>
  <tile id="6">
+  <properties>
+   <property name="collides" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="13">
   <properties>
    <property name="collides" type="bool" value="true"/>
   </properties>
