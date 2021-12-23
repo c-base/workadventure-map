32cc3d3f55d2dbe027c1396fd839682d762b7cab
Sven G. Brönstrup <broenstrup@spom.net>
2021-12-23 20:44:33 +0100

    Added kolle anim

--- tiles/labels.tsx
+++ tiles/labels.tsx
@@ -1,13 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="labels" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+<tileset version="1.5" tiledversion="1.7.2" name="labels" tilewidth="32" tileheight="32" tilecount="512" columns="16">
  <properties>
   <property name="tilesetCopyright">Tileset 'labels' created by c-base members
 - C3WOC logo by C3WOC
 - Don't panic poster and some warning signs by e-punc
 - cyber sticker by wizz
+- circle animation by Kolle
 - other stuff and compilation by StarPeak</property>
  </properties>
- <image source="../img/lables.png" width="512" height="512"/>
+ <image source="../img/lables.png" width="512" height="1024"/>
  <tile id="143">
   <animation>
    <frame tileid="143" duration="10000"/>
@@ -18,6 +19,24 @@
    <frame tileid="140" duration="50"/>
    <frame tileid="141" duration="50"/>
    <frame tileid="142" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="268">
+  <animation>
+   <frame tileid="268" duration="150"/>
+   <frame tileid="269" duration="150"/>
+   <frame tileid="270" duration="150"/>
+   <frame tileid="271" duration="150"/>
+   <frame tileid="284" duration="150"/>
+   <frame tileid="285" duration="150"/>
+   <frame tileid="286" duration="150"/>
+   <frame tileid="287" duration="150"/>
+   <frame tileid="300" duration="150"/>
+   <frame tileid="301" duration="150"/>
+   <frame tileid="302" duration="150"/>
+   <frame tileid="303" duration="150"/>
+   <frame tileid="316" duration="150"/>
+   <frame tileid="317" duration="150"/>
   </animation>
  </tile>
 </tileset>
